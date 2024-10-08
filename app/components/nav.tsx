d0e5a5f464fcd37cf144844db4ce756809cedecb
101045330 <101045330@georgebrown.ca>
2024-10-08 17:36:51 +0000

    html template header nav sepatared as reusable component

---
+++ app/components/nav.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Nav(){
+    return <>
+        <div className="col-3">
+            <div className="list-group">
+
+                <a href="home.html" className="list-group-item list-group-item-action">
+                    Home
+            ``    </a>
+
+                <a href="about.html" className="list-group-item list-group-item-action">
+                    About Us
+                </a>
+``
+                <a href="contact.html" className="list-group-item list-group-item-action">
+                    Contact Us
+                </a>
+                
+            </div>
+        </div>
+       </>
+
+}
